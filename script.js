9142d553d976a232742266d2cc6488aa516f9c8d
Jose Orozco <jose816orozco@gmail.com>
2021-04-24 18:52:21 -0500

    js weather data

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
+
+    
 var Key = "199e10c35563a5ea2bc31cba23e1d3f9";
 
+
+
 function getCurrentWeather(city) {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=imperial&appid=" +
-      Key
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      document.querySelector(".city").innerHTML = data.name;
-      console.log(data.weather[0]);
+    
+    
+    
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        Key
+        )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            document.querySelector(".city").innerHTML = data.name;
+            console.log(data.weather[0].icon);
+            
+            document.querySelector(".current-weather").innerHTML = 
+            "<h3>" + data.weather[0].description +  "</h3>" +
+            "<p>" + data.weather[0].main + "</p>" +
+            document.querySelector(".icons").innerHTML = data.weather[0].icon;
 
-      document.querySelector(".current-weather").innerHTML =
-        "<h3>" +
-        data.weather[0].description +
-        "</h3>" +
-        "<p>" +
-        data.weather[0].main +
-        "</p>";
-      document.querySelector(".icons").innerHTML = data.weather[0].icon;
-
-      getForcast(data.coord.lat, data.coord.lon);
-    });
-}
-
-function getForcast(lat, lon) {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&units=imperial&appid=" +
-      Key
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-
-getCurrentWeather("kansas city");
+            document.querySelector('.temp').innerHTML =
+            "<p>" + data.main.temp + "</p>"
+            console.log(data.main.temp);
+            
+            
+            getForcast(data.coord.lat, data.coord.lon);
+            
+        });
+        
+    }
+    
+    function getForcast(lat, lon) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            "&units=imperial&appid=" +
+            Key
+            )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+            
+        }
+        
+        getCurrentWeather("kansas city");
+        
+        
+        
+        const storageInput = document.querySelector('.storage');
+        const text = document.querySelector('.text');
+        const button = document.querySelector('.button');
+        
+        storageInput.addEventListener('input', letter => {
+            text.textContent = letter.target.value
+            
+        })
+        
+        const saveStorage = () => {
+            localStorage.setItem('textinput', text.textContent) 
+            button.addEventListener('click',saveStorage)
+        }
+        
+        
+        
